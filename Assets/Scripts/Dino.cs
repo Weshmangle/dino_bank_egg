044ac9ff6b8dac0d483d28e578cb1ea3974ae5a5
AntiEdge <antoine.bouilliez@hotmail.com>
2022-05-04 11:20:07 +0200

    Add NavMesh

---
+++ Assets/Scripts/Dino.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dino : MonoBehaviour
+{
+    [SerializeField]private float m_speed;
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        //Movement();
+    }
+
+    private void Movement()
+    {
+        _rigidbody.velocity = Vector3.forward * m_speed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _objectif;
+    [SerializeField] private NavMeshAgent _agent;
+    private void Start()
+    {
+        _agent.SetDestination(_objectif.transform.position);
+    }
+}

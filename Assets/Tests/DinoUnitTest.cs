cb8bdb52aadae1208bcbe9cd02d2c4cb030fbf2b
Caudron David-Emmanuel <96061077+DavidECaudron@users.noreply.github.com>
2022-05-05 15:35:51 +0200

    Unit Test Shop

--- Assets/Tests/DinoUnitTest.cs
+++ Assets/Tests/DinoUnitTest.cs
@@ -7,50 +7,5 @@ using DinoSpace;
 
 public class DinoUnitTest
 {
-    List<DataDino> _listOfDinos;
-    [SetUp]
-    public void Setup()
-    {
-
-    }
-
-    /*Spawn MANAGER*/
-
-    //SpawnDino
-
-
-    /*SHOP*/
-
-    //GetNextDino
-
-    //BuyNextDino
-
-    //SortListByCost
-
-
-    /*GAME MANAGER*/
-
-    //AddDino
-
-    //GoToDestination
-
-
-    /*CONTAINER*/
-
-    //AddDinoToContainer
-
-    //IsFull
-
-    //NextCost
-
-    //LevelUp
-
-    [UnityTest]
-    public IEnumerator DinoUnitTestWithEnumeratorPasses()
-    {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
-    }
 
 }

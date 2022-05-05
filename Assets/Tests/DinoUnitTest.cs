d54489f6dc153c68374f38c55822034b0cbd8d03
Caudron David-Emmanuel <96061077+DavidECaudron@users.noreply.github.com>
2022-05-05 16:28:57 +0200

    TestDino

--- Assets/Tests/DinoUnitTest.cs
+++ Assets/Tests/DinoUnitTest.cs
@@ -7,11 +7,17 @@ using DinoSpace;
 
 public class DinoUnitTest
 {
-    List<DataDino> _listOfDinos;
+    List<DataDino> _listOfDinos; 
+    Container container;
     [SetUp]
     public void Setup()
     {
-
+        container = new GameObject().AddComponent<Container>();
+        container.data = ScriptableObject.CreateInstance<DataContainer>();
+        container.data.limit = 50;
+        container.data.cost = 1.75f;
+        container.currentCountDino = 0;
+        container.limit = container.data.limit;
     }
 
     /*Spawn MANAGER*/
@@ -38,19 +44,56 @@ public class DinoUnitTest
     /*CONTAINER*/
 
     //AddDinoToContainer
-
-    //IsFull
-
-    //NextCost
-
-    //LevelUp
-
-    [UnityTest]
-    public IEnumerator DinoUnitTestWithEnumeratorPasses()
+    
+    [Test]
+    public void ContainerIsFullAfterAddMMaxDinoUnitTestWithEnumeratorPasses()
     {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
+        container.currentCountDino = 0;
+        for (int i = 0; i < container.limit; i++)
+        {
+            container.AddDinoToContainer();
+        }
+        Assert.IsTrue(container.isFull(), "Container is not Full"); 
+    }
+    
+   [Test]
+    public void ContainerNotIsFullAfterAdd2DinoUnitTestWithEnumeratorPasses()
+    {
+        container.currentCountDino = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            container.AddDinoToContainer();
+        }
+        Assert.IsFalse(container.isFull());
+    }
+    
+    [Test]
+    public void ContainerIsFullWhenAddOver50Dinos()
+    {
+        container.currentCountDino = 0;
+        try
+        { 
+            for (int i = 0; i < container.limit + 10; i++)
+            {
+                container.AddDinoToContainer();
+            }
+        }
+        catch(UnityException ex)
+        {
+        }
+        Assert.IsTrue(container.isFull());
     }
 
+    //[Test]
+    public void ContainerLevelUp()
+    {
+        container.cost = 10;
+        container.data.factorCost = 1.75f;
+
+        var nextCost = container.NextCost();
+
+        container.LevelUp();
+
+        Assert.IsTrue(container.cost == nextCost, "NextCost not matching expected value");
+    }
 }
